fe49ec8164f2ac3647fc66de349138959f88b461
Matej Hočevar <matej.hocevar@tretton37.com>
2023-10-02 19:21:51 +0200

    feat: define theme seed colors

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,3 +1,4 @@
+import 'package:flex_seed_scheme/flex_seed_scheme.dart';
 import 'package:flutter/material.dart';
 
 import 'sample_feature/sample_item_details_view.dart';
@@ -15,6 +16,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const Color primarySeedColor = Color(0xFF3DB870);
+    const Color secondarySeedColor = Color(0xFF000004);
+    const Color tertiarySeedColor = Color(0xFF21418C);
+    final ColorScheme schemeLight = SeedColorScheme.fromSeeds(
+      brightness: Brightness.light,
+      primaryKey: primarySeedColor,
+      secondaryKey: secondarySeedColor,
+      tertiaryKey: tertiarySeedColor,
+      tones: FlexTones.vivid(Brightness.light),
+    );
+    final ColorScheme schemeDark = SeedColorScheme.fromSeeds(
+      brightness: Brightness.dark,
+      primaryKey: primarySeedColor,
+      secondaryKey: secondarySeedColor,
+      tertiaryKey: tertiarySeedColor,
+      tones: FlexTones.vivid(Brightness.dark),
+    );
+
     return ListenableBuilder(
       listenable: settingsController,
       builder: (BuildContext context, Widget? child) {
@@ -25,9 +44,15 @@ class MyApp extends StatelessWidget {
             Locale('en', ''),
           ],
           onGenerateTitle: (_) => 'Knowabunga 2023',
-          theme: ThemeData(),
-          darkTheme: ThemeData.dark(),
           themeMode: settingsController.themeMode,
+          theme: ThemeData(
+            colorScheme: schemeLight,
+            useMaterial3: true,
+          ),
+          darkTheme: ThemeData(
+            colorScheme: schemeDark,
+            useMaterial3: true,
+          ),
           onGenerateRoute: (RouteSettings routeSettings) {
             return MaterialPageRoute<void>(
               settings: routeSettings,
