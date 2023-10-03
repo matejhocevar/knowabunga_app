7bfd960751d641166b4d74fb59325f887b060797
Matej Hočevar <matej.hocevar@tretton37.com>
2023-10-03 02:38:55 +0200

    fix: fix activity details scroll bug

--- lib/src/activity_feature/activity_details_view.dart
+++ lib/src/activity_feature/activity_details_view.dart
@@ -46,85 +46,82 @@ class ActivityDetailsView extends StatelessWidget {
           ],
         ),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(screenWidth / 10),
-        child: ListView(
-          shrinkWrap: true,
-          children: [
-            const SizedBox(height: 32),
-            Align(
-              alignment: Alignment.topCenter,
-              child: SizedBox(
-                width: min(screenWidth / 2, 400),
-                child: ActivityThumbnail(
-                  image: activity.image,
-                  tag: activity.tag,
-                  size: min(screenWidth / 2, 400),
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: screenWidth / 10),
+        shrinkWrap: true,
+        children: [
+          const SizedBox(height: 32),
+          Align(
+            alignment: Alignment.topCenter,
+            child: SizedBox(
+              width: min(screenWidth / 2, 400),
+              child: ActivityThumbnail(
+                image: activity.image,
+                tag: activity.tag,
+                size: min(screenWidth / 2, 400),
+              ),
+            ),
+          ),
+          const SizedBox(height: 8),
+          if (activity.speakers.isNotEmpty) ...[
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 124),
+              child: Text(
+                activity.speakers.smartJoin(),
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 18,
                 ),
               ),
             ),
             const SizedBox(height: 8),
-            if (activity.speakers.isNotEmpty) ...[
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 124),
-                child: Text(
-                  activity.speakers.smartJoin(),
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    fontSize: 18,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 8),
-            ],
-            Text(
-              activity.title,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 8),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                IconText(
-                  icon: const Icon(Icons.access_time, size: 16),
-                  text: '${activity.timeStart} - ${activity.timeEnd}',
-                ),
-                const SizedBox(width: 8),
-                IconText(
-                  icon: const Icon(Icons.location_on_outlined, size: 16),
-                  text: activity.location,
-                ),
-              ],
-            ),
-            const SizedBox(height: 8),
-            Wrap(
-              alignment: WrapAlignment.center,
-              spacing: 8,
-              children: <Widget>[
-                ...activity.tags
-                    .map(
-                      (t) => ActivityChip(
-                        text:
-                            t.split('.').last.replaceAll('_', ' ').capitalize(),
-                      ),
-                    )
-                    .toList(),
-              ],
-            ),
-            const SizedBox(height: 16),
-            Text(
-              activity.description,
-              style: const TextStyle(
-                fontSize: 14,
-              ),
-            ),
-            const SizedBox(height: 64),
           ],
-        ),
+          Text(
+            activity.title,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 8),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              IconText(
+                icon: const Icon(Icons.access_time, size: 16),
+                text: '${activity.timeStart} - ${activity.timeEnd}',
+              ),
+              const SizedBox(width: 8),
+              IconText(
+                icon: const Icon(Icons.location_on_outlined, size: 16),
+                text: activity.location,
+              ),
+            ],
+          ),
+          const SizedBox(height: 8),
+          Wrap(
+            alignment: WrapAlignment.center,
+            spacing: 8,
+            children: <Widget>[
+              ...activity.tags
+                  .map(
+                    (t) => ActivityChip(
+                      text: t.split('.').last.replaceAll('_', ' ').capitalize(),
+                    ),
+                  )
+                  .toList(),
+            ],
+          ),
+          const SizedBox(height: 16),
+          Text(
+            activity.description,
+            style: const TextStyle(
+              fontSize: 14,
+            ),
+          ),
+          const SizedBox(height: 64),
+        ],
       ),
     );
   }
